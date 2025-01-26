ca20e6bfd1ca8f94c02f232b032c4be9e1e2e350
Jitendra Pratap <jitendrapratap432@gmail.com>
2025-01-26 11:59:24 +0530

    updated

--- src/Screens/Cart.jsx
+++ src/Screens/Cart.jsx
@@ -1,34 +1,17 @@
 import React,{useEffect, useState} from 'react'
-import axios from 'axios';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 function Cart() {
-  const [productData, setProductData] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://fakestoreapi.com/products')
-      .then(response => {
-        setProductData(response.data);
-      })
-      .catch(error => {
-        console.log('Error fetching data:', error);
-      });
-  }, []);
+  
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.headline}>Products List</Text>
-      <FlatList
-        data={productData}
-        keyExtractor={(item) => item.id.toString()} // Ensure unique key
-        renderItem={({ item }) => (
-          <View style={styles.productItem}>
-            <Text style={styles.productName}>{item.title}</Text>
-            <Text style={styles.productPrice}>${item.price}</Text>
-          </View>
-        )}
-      />
+
+    <View>
+      <Text>
+        Cart
+      </Text>
     </View>
+    
   );
 }
 
@@ -40,23 +23,5 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: '#fff',
   },
-  headline: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'red',
-    marginBottom: 10,
-  },
-  productItem: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  productName: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  productPrice: {
-    fontSize: 16,
-    color: 'green',
-  },
+ 
 })
